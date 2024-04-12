f94fb088b105cbb599d35f1584488e82f1819651
Vladislav <vladisslav.f.e@gmail.com>
2024-04-12 17:11:57 +0300

    Version 2.0

--- Program.py
+++ Program.py
@@ -1,19 +1,10 @@
-xA = int(input('Enter X of A '))
-yA = int(input('Enter Y of A '))
-xB = int(input('Enter X of B '))
-yB = int(input('Enter Y of B '))
-xC = int(input('Enter X of C '))
-yC = int(input('Enter Y of C '))
-xD = int(input('Enter X of D '))
-yD = int(input('Enter Y of D '))
-xE = int(input('Enter X of E '))
-yE = int(input('Enter Y of E '))
+x1, y1, x2, y2 = map(int, input('Координаты прямоугольника (через пробел)').split())
+point_x, point_y = map(int, input('Координаты точки (через пробел)').split())
+rect = ((x1,y1),(x2,y2))
 bool = False
 
-if yA != yB or xA != xD or xC != xB or yC != yD:
-    print('Not a rectangle')
-
+if point_x <= x2 and point_x >= x1 and point_y <= y1 and point_y >= y2:
+    bool = True
+    print('Point is inside: ', bool)
 else:
-    if xE < xB and xE > xA and yE < yA and yE > yD:
-        bool = True
-        print('Point is inside: ', bool)
+    print('Not inside')
